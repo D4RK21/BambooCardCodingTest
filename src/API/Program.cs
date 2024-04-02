5b7eba4e7cfef62845fe2e80a45c8e27b8637f14
D4RK21 <zhmaytsevm@gmail.com>
2024-04-02 23:10:39 +0200

    Created base entities and added Refit client

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,6 +1,15 @@
+using Infrastructure;
+using Refit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRefitClient<IApiProvider>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.Configuration["BaseApiUrl"]));
+
+// ToDo: Move in the separate file
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
